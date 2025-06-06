88992628d0472d039832e7e9d399ab3e53135191
Artyom Obshatko <aobshatk@c4r1s5.42warsaw.pl>
2025-06-06 14:59:24 +0200

    waiting for cd to finish and then ready for test

--- builtins/echo.c
+++ builtins/echo.c
@@ -6,93 +6,121 @@
 /*   By: svolkau <gvardovski@icloud.com>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/18 09:17:17 by svolkau           #+#    #+#             */
-/*   Updated: 2025/05/26 18:00:01 by svolkau          ###   ########.fr       */
+/*   Updated: 2025/06/06 13:14:23 by svolkau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-void readprintdir(char *str)
+void	printarr(char **gv, int start, int mes)
 {
-    DIR *dir;
-    struct dirent *dirs;
-	
-    dir = opendir(str);
-    if(dir != NULL)
-    {
-        while((dirs = readdir(dir)) != NULL)
-        {
-            if ((ft_strncmp(".", dirs->d_name, ft_strlen(dirs->d_name)) != 0) 
-			&& (ft_strncmp("..", dirs->d_name, ft_strlen(dirs->d_name)) != 0))
-            	ft_printf("%s ", dirs->d_name);
-        }
-    }
-    else
-        ft_printf("minishell: echo: Cannot open directory '%s'\n", str);
-    closedir(dir);
-}
-
-char *cwd()
-{
-	char buf[1024];
-	char *p;
-	
-    p = getcwd(buf, 1024);
-	if (!p)
-	{
-		perror("minishell: pwd");
-		return (NULL);
-	}
-	return(ft_strdup(p));
-}
-
-void print_arr(char **gv, int start, int mes)
-{
-	int i;
-	char *str;
+	int	i;
 
 	i = start;
-	while(gv[i])
+	while (gv[i])
 	{
-		if ((ft_strncmp("*", gv[i], ft_strlen(gv[i])) == 0))
-		{
-			str = cwd();
-			readprintdir(str);
-			free(str);
-			if (i < (arr_len(gv)- 1))
-				i++;
-		}
-		if ((ft_strncmp("*", gv[i], ft_strlen(gv[i])) != 0))
-			printf("%s", gv[i]);
-		if (i != (arr_len(gv) - 1))
-			ft_printf("");
+		if ((i == start) && (ft_strncmp(" ", gv[i], ft_strlen(gv[i])) == 0))
+			i++;
+		if (gv[i] == NULL)
+			break ;
+		printarreach(gv, i);
 		i++;
 	}
 	if (mes == 0)
 		ft_printf("\n");
 }
 
-int ft_echo(t_main_dat *main_data, char **gv)
+int	checkifcmdexist(char **gv, int i)
 {
-	t_main_dat temp;
+	char	*trimstr;
+	char	*catstr;
+	char	*tmp;
+
+	if (ft_strncmp(" ", gv[i], ft_strlen(gv[i])) != 0)
+	{
+		catstr = ft_strdup(gv[0]);
+		while (gv[i] && ft_strncmp(" ", gv[i], ft_strlen(gv[i])) != 0)
+		{
+			trimstr = trimall(gv[i++]);
+			tmp = ft_strjoin(catstr, trimstr);
+			free(catstr);
+			free(trimstr);
+			catstr = tmp;
+		}
+		if (checkcmd(catstr, 1) == 1)
+			return (-1);
+		free(catstr);
+	}
+	return (i);
+}
+
+int	gotofirstspace(char **gv, int i)
+{
+	int	s;
+	int	q;
+
+	s = 0;
+	q = 0;
+	while (gv[i] != NULL && (ft_strncmp(" ", gv[i], ft_strlen(gv[i]) == 0)
+			|| ft_strncmp("\"\"", gv[i], ft_strlen(gv[i])) == 0
+			|| ft_strncmp("''", gv[i], ft_strlen(gv[i])) == 0))
+	{
+		if (ft_strncmp(" ", gv[i], ft_strlen(gv[i])) == 0)
+		{
+			if (s == 1 && q == 1)
+				break ;
+			s = 1;
+		}
+		if ((ft_strncmp("\"\"", gv[i], ft_strlen(gv[i])) == 0)
+			|| (ft_strncmp("''", gv[i], ft_strlen(gv[i])) == 0))
+			q = 1;
+		i++;
+	}
+	return (i);
+}
+
+void	mainecho(char **gv, int i)
+{
+	int		len;
+	char	*trimstr;
+
+	trimstr = trimall(gv[i]);
+	len = ft_strlen(trimstr);
+	if (len == 0)
+		len += 1;
+	if (ft_strncmp("-n", trimstr, len) == 0 && (arr_len(gv) - 1) == i)
+		ft_printf("");
+	if (ft_strncmp("-n", trimstr, len) == 0)
+		printarr(gv, i + 1, 1);
+	if (ft_strncmp("-n", trimstr, len) != 0)
+		printarr(gv, i, 0);
+	free(trimstr);
+}
+
+int	ft_echo(t_main_dat *main_data, char **gv)
+{
+	t_main_dat	temp;
+	int			i;
 
 	temp = *main_data;
 	(void)temp;
-	if (ft_strlen(gv[0]) > 4)
-	{
-		ft_printf("minishell: '%s': command not found\n", gv[0]);
-		return(1);	
-	}
-    if (arr_len(gv) == 1)
+	if (checkcmd(gv[0], 0) == 1)
+		return (1);
+	if (arr_len(gv) == 1)
 	{
 		ft_printf("\n");
-		return(0);
+		return (0);
 	}
-   	if ((ft_strncmp("-n", gv[1], ft_strlen(gv[1])) == 0) && (arr_len(gv) == 2))
-	   	ft_printf("");
-	if ((ft_strncmp("-n",gv[1], ft_strlen(gv[1])) == 0) && (arr_len(gv) > 2))
-	   	print_arr(gv, 2, 1);
-	if (ft_strncmp("-n",gv[1], ft_strlen(gv[1])) != 0)
-		print_arr(gv, 1, 0);
-	return(0);
+	i = 1;
+	i = checkifcmdexist(gv, i);
+	if (i < 0)
+		return (1);
+	i = gotofirstspace(gv, i);
+	if (gv[i] == NULL)
+	{
+		ft_printf("\n");
+		return (0);
+	}
+	mainecho(gv, i);
+	return (0);
 }
